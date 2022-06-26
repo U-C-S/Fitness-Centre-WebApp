566527b69ce3bf402ecb4c5fcf34522733fb3fd1
u-c-s <uchanakyasrinivas@gmail.com>
2022-06-27 00:03:31 +0530

    update user route

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,12 +1,37 @@
 import { FastifyInstance } from "fastify";
+import { jwtUserPayload } from "../plugins/jwt-auth";
 
 export function userRoutes(fastify: FastifyInstance) {
   let { prisma } = fastify;
 
-  fastify.get("/", async (request, reply) => {
-    const profiles = await prisma.user.findMany();
-    return profiles;
-  });
+  fastify.get(
+    "/",
+    {
+      onRequest: [fastify.authenticate],
+    },
+    async (request, reply) => {
+      let id = (request.user as jwtUserPayload).id;
+      const profile = await prisma.user.findUnique({ where: { id } });
+
+      if (!profile) {
+        return reply.code(404).send({ message: "Profile not found" });
+      }
+
+      let { fname, age, height, lname, weight, ph_num, address } = profile;
+      reply.code(200).send({
+        succes: true,
+        data: {
+          fname,
+          age,
+          height,
+          lname,
+          weight,
+          ph_num,
+          address,
+        },
+      });
+    }
+  );
 
   fastify.post("/", async (request, reply) => {
     const { body } = request as any;
