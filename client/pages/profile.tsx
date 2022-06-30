765c953c45c5b3917685cbfb00f18aec318f0a4c
u-c-s <uchanakyasrinivas@gmail.com>
2022-07-01 01:36:51 +0530

    improve user profile page

--- client/pages/profile.tsx
+++ client/pages/profile.tsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { Avatar, Divider, Image, Paper, Title, Text, Loader } from "@mantine/core";
 import useSWR from "swr";
 import { userProfileFetcher } from "../utils/fetcher";
+import { Plan } from "../components/Plans";
+import { PlansData } from "../utils/plandata";
 
 export default function Profile() {
 	const { data } = useSWR(process.env.API_URL + "/api/user/", userProfileFetcher);
@@ -11,22 +13,37 @@ export default function Profile() {
 	}
 
 	return (
-		<Paper radius="md" p={0} withBorder shadow={"lg"} style={{ width: "500px", margin: "0 auto" }}>
-			<Avatar
-				size={150}
-				radius={"xl"}
-				style={{ margin: "auto" }}
-				src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
-				alt="it's me"
-			/>
+		<>
+			{data.data.training_info ? (
+				<></>
+			) : (
+				<div>
+					<Title align="center" order={2} m={15}>
+						--- You might need to buy a plan ---
+					</Title>
+					<Divider />
 
-			<Divider my="sm" />
-			<p>Name: {data.data.name}</p>
-			<p>age: {data.data.age}</p>
-			<p>height: {data.data.height}</p>
-			<p>weight: {data.data.weight}</p>
-			<p>Gender: {data.data.gender}</p>
-			<p>Phone Number: {data.data.ph_num}</p>
-		</Paper>
+					<div style={{ margin: "20px auto", width: "75%", display: "flex", flexWrap: "wrap" }}>
+						{PlansData.map(plan => (
+							<Plan plan={plan} />
+						))}
+					</div>
+					<Divider />
+				</div>
+			)}
+			<Paper radius="md" p={5} withBorder shadow={"lg"} style={{ width: "400px", margin: "5px auto" }}>
+				<Title align="center" order={2}>
+					Your Profile
+				</Title>
+				<p>Name: {data.data.name}</p>
+				<p>age: {data.data.age}</p>
+				<p>height: {data.data.height}</p>
+				<p>weight: {data.data.weight}</p>
+				<p>Gender: {data.data.gender}</p>
+				<p>Phone Number: {data.data.ph_num}</p>
+				<Divider />
+				{data.data.training_info ? <p>Your Trainer is: {data.data.training_info.trainer.name}</p> : <></>}
+			</Paper>
+		</>
 	);
 }
